f41ae1b48de4897d10456a6fda27dd9f31f7c39f
tonyycsw-lgtm <tonyy.csw@gmail.com>
2026-03-06 15:01:18 +0800

    Add files via upload

---
+++ data/food.js
@@ -0,0 +1,49 @@
+// data/food.js
+window.cardsData = window.cardsData || {};
+window.cardsData.food = [
+    { 
+        word: "香蕉", 
+        desc: "香蕉",
+        example1: "食香蕉補充能量",
+        example2: "香蕉好甜",
+        img: "images/food_香蕉.png",
+        audioCantonese: "audio/food_香蕉_粵.mp3",
+        audioMandarin: "audio/food_香蕉_普.mp3"
+    },
+    { 
+        word: "蘋果", 
+        desc: "蘋果",
+        example1: "一日一蘋果",
+        example2: "蘋果有益",
+        img: "images/food_蘋果.png",
+        audioCantonese: "audio/food_蘋果_粵.mp3",
+        audioMandarin: "audio/food_蘋果_普.mp3"
+    },
+    { 
+        word: "橙", 
+        desc: "橙",
+        example1: "橙含有維他命C",
+        example2: "鮮搾橙汁",
+        img: "images/food_橙.png",
+        audioCantonese: "audio/food_橙_粵.mp3",
+        audioMandarin: "audio/food_橙_普.mp3"
+    },
+    { 
+        word: "菠蘿包", 
+        desc: "菠蘿包",
+        example1: "新鮮出爐菠蘿包",
+        example2: "下午茶食菠蘿包",
+        img: "images/food_菠蘿包.png",
+        audioCantonese: "audio/food_菠蘿包_粵.mp3",
+        audioMandarin: "audio/food_菠蘿包_普.mp3"
+    },
+    { 
+        word: "蛋撻", 
+        desc: "蛋撻",
+        example1: "熱辣辣蛋撻",
+        example2: "港式蛋撻",
+        img: "images/food_蛋撻.png",
+        audioCantonese: "audio/food_蛋撻_粵.mp3",
+        audioMandarin: "audio/food_蛋撻_普.mp3"
+    }
+];
